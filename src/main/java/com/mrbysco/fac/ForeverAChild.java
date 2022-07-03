f8a28aeabb22d3b70a84a56780ccfbeb1dc82427
Mrbysco <Mrbysco@users.noreply.github.com>
2022-07-03 03:24:54 +0200

    Port to 1.19 [push]

--- src/main/java/com/mrbysco/fac/ForeverAChild.java
+++ src/main/java/com/mrbysco/fac/ForeverAChild.java
@@ -10,8 +10,6 @@ import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Mod(ForeverAChild.MOD_ID)
 public class ForeverAChild {
@@ -19,8 +17,6 @@ public class ForeverAChild {
 	public static final ResourceLocation AGELOCKED_CAP = new ResourceLocation(MOD_ID, "capability.agelocked");
 	public static final TagKey<Item> AGE_LOCKING_TAG = ItemTags.create(new ResourceLocation(MOD_ID, "age_locking"));
 	public static final TagKey<Item> AGE_UNLOCKING_TAG = ItemTags.create(new ResourceLocation(MOD_ID, "age_unlocking"));
-
-	public static final Logger LOGGER = LogManager.getLogger();
 
 	public ForeverAChild() {
 		IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
