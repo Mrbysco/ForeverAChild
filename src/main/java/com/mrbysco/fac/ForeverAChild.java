2031321b54c17db992e270bd9b34a4e443942bdc
Mrbysco <Mrbysco@users.noreply.github.com>
2022-08-19 21:37:21 +0200

    Update forge and make use of `AutoRegisterCapability`

--- src/main/java/com/mrbysco/fac/ForeverAChild.java
+++ src/main/java/com/mrbysco/fac/ForeverAChild.java
@@ -1,12 +1,10 @@
 package com.mrbysco.fac;
 
-import com.mrbysco.fac.capability.ILocked;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -20,12 +18,7 @@ public class ForeverAChild {
 
 	public ForeverAChild() {
 		IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-		eventBus.addListener(this::registerCapabilities);
 
 		MinecraftForge.EVENT_BUS.register(new CapabilityHandler());
-	}
-
-	public void registerCapabilities(RegisterCapabilitiesEvent event) {
-		event.register(ILocked.class);
 	}
 }
--- src/main/java/com/mrbysco/fac/capability/ILocked.java
+++ src/main/java/com/mrbysco/fac/capability/ILocked.java
@@ -1,5 +1,8 @@
 package com.mrbysco.fac.capability;
 
+import net.minecraftforge.common.capabilities.AutoRegisterCapability;
+
+@AutoRegisterCapability
 public interface ILocked {
 	boolean isLocked();
 
