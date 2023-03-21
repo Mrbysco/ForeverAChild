444faeb379cc004b23fb01dc6c3cfe319dc561ac
Mrbysco <Mrbysco@users.noreply.github.com>
2023-03-21 19:30:33 +0100

    Update to 1.19.4 [build] [publish]

--- src/main/java/com/mrbysco/fac/ForeverAChild.java
+++ src/main/java/com/mrbysco/fac/ForeverAChild.java
@@ -5,9 +5,7 @@ import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(ForeverAChild.MOD_ID)
 public class ForeverAChild {
@@ -17,8 +15,6 @@ public class ForeverAChild {
 	public static final TagKey<Item> AGE_UNLOCKING_TAG = ItemTags.create(new ResourceLocation(MOD_ID, "age_unlocking"));
 
 	public ForeverAChild() {
-		IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
 		MinecraftForge.EVENT_BUS.register(new CapabilityHandler());
 	}
 }
