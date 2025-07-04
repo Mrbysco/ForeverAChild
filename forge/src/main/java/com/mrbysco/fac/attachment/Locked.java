6773681cee031d9a95b6bb491d91cc711ea903aa
Mrbysco <Mrbysco@users.noreply.github.com>
2025-07-04 18:09:31 +0200

    Port to 1.21.7

--- forge/src/main/java/com/mrbysco/fac/attachment/Locked.java
+++ forge/src/main/java/com/mrbysco/fac/attachment/Locked.java
@@ -4,21 +4,20 @@ import net.minecraft.world.level.storage.ValueInput;
 import net.minecraft.world.level.storage.ValueOutput;
 import net.neoforged.neoforge.attachment.IAttachmentHolder;
 import net.neoforged.neoforge.attachment.IAttachmentSerializer;
+import org.jetbrains.annotations.NotNull;
 
 public class Locked implements IAttachmentSerializer<Boolean> {
 	public static final Locked INSTANCE = new Locked();
 
+	@NotNull
 	@Override
-	public Boolean read(IAttachmentHolder holder, ValueInput input) {
+	public Boolean read(@NotNull IAttachmentHolder holder, ValueInput input) {
 		return input.getBooleanOr("locked", false);
 	}
 
 	@Override
-	public boolean write(Boolean locked, ValueOutput output) {
-		if (locked != null) {
-			output.putBoolean("locked", locked);
-			return true;
-		}
-		return false;
+	public boolean write(@NotNull Boolean locked, @NotNull ValueOutput output) {
+		output.putBoolean("locked", locked);
+		return true;
 	}
 }
